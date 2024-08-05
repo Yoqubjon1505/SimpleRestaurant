83b26d86f7276e67f8d9c2065564a37f63b06170
Yo.Akhmedov@gmail.com <Yo.Akhmedov@gmail.com>
2024-08-05 23:10:27 +0500

    Created forms

--- RestaurantSimulation_1/Form1.Designer.cs
+++ RestaurantSimulation_1/Form1.Designer.cs
@@ -29,24 +29,149 @@
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
+            panel1 = new Panel();
+            button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            button2 = new Button();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(33, 24);
+            textBox1.Location = new Point(28, 104);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(74, 29);
+            textBox1.Size = new Size(110, 29);
             textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(28, 21);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(169, 64);
+            panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(160, 104);
+            button1.Name = "button1";
+            button1.Size = new Size(191, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Submit new request";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(15, 31);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(68, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Chicken";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(109, 31);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(45, 19);
+            radioButton2.TabIndex = 0;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Egg";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(28, 167);
+            button2.Name = "button2";
+            button2.Size = new Size(323, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Copy the previous request";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(28, 223);
+            button3.Name = "button3";
+            button3.Size = new Size(323, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Prepare Food";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(28, 282);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(323, 131);
+            textBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 1);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Menu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 264);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Results:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(213, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Egg Quality:  ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(284, 37);
+            label4.Name = "label4";
+            label4.Size = new Size(12, 15);
+            label4.TabIndex = 1;
+            label4.Text = "?";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(392, 502);
+            ClientSize = new Size(364, 430);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(textBox2);
             Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -54,5 +179,16 @@
         #endregion
 
         private TextBox textBox1;
+        private Panel panel1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
